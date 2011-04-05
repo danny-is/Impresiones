85cfb5053cbd9eaa0449ab9ebe014e327b6cf1c5
Roberto Rodriguez <rrodriguez@incompanysolutions.com>
2011-04-05 09:50:28 -0500

    added print.haml

--- init.rb
+++ init.rb
@@ -5,13 +5,42 @@ require 'ninesixty'
 require 'hpricot'
 require 'open-uri'
 
-get '/print' do
-@test = params['codes'];
-
-haml :index
-
+def render_file(filename)
+  contents = File.read('views/'+filename+'.haml')
+  Haml::Engine.new(contents).render
 end
 
+get '/print' do
+  codes = params['codes'];
+  arr = codes.split(",")
+  
+   @codes = []
+ 	 @names = []
+ 	 @descriptions = []
+ 	 @images = []
+   @prices = []
+   
+   f =  open("http://localhost:9393/productfeed.xml") do |f|
+   doc = Hpricot.XML(f)
+     i=0
+    arr.each do |code|
+       t = '//code[text()="' + code + '"]/..'
+       
+       (doc / t ) .each do |p|
+       @codes[i] = (p/"code").inner_html
+       @names[i]= (p/"name").inner_html
+       @descriptions[i]= (p/"description").inner_html
+       @images[i]= (p/"image_small").inner_html 
+       @prices[i]= (p/"sellprice_us").inner_html
+       i+=1
+     end
+   end
+   
+   
+   end
+  
+   haml :print
+end
 
 get '/' do
   
@@ -30,7 +59,6 @@ get '/' do
 	
   @n = (@doc/"product").length #tamaño 
  	 	
- 	 	
  	for i in 0..@n -1 do
  	  @codes[i]= (@doc/"code")[i].inner_html
 		@names[i]= (@doc/"name")[i].inner_html
@@ -38,12 +66,10 @@ get '/' do
 		@images[i]= (@doc/"image_small")[i].inner_html 
 		@prices[i]= (@doc/"sellprice_us")[i].inner_html
 		end
- 
-  haml :index
+ haml :index
+  
   end
  
 
  
 end
-
-
