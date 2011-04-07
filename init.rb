8536ee9a18fe913d6d5f1c7d6dd87fb5a314d996
Luis <lquirosr@gmail.com>
2011-04-07 14:34:43 -0600

    agregado el parseo de el feed obtenido por medio de wsdl

--- init.rb
+++ init.rb
@@ -4,11 +4,41 @@ require 'haml'
 require 'ninesixty'
 require 'hpricot'
 require 'open-uri'
+require 'soap/wsdlDriver'
 
 def render_file(filename)
   contents = File.read('views/'+filename+'.haml')
   Haml::Engine.new(contents).render
 end
+
+
+def object_import(xml_data)
+		
+		product_hash = {}
+		n = xml_data.product_ListRetrieveResult.products.length
+		for i in 0..n -1 do
+		
+		product_hash[i] = {
+			"titulo" => xml_data.product_ListRetrieveResult.products[i].productTitle ,
+			"especs" => xml_data.product_ListRetrieveResult.products[i].custom1,
+			"precio" => xml_data.product_ListRetrieveResult.products[i].pricesSaleArray.string,
+		  "imagen" => xml_data.product_ListRetrieveResult.products[i].smallImage}
+
+		end
+	return product_hash
+end
+
+get '/connect' do
+  ENV['SHOWSOAP'] = 'true'
+  url = 'http://www.btc.cr/CatalystWebService/CatalystEcommerceWebservice.asmx?WSDL'
+	driver = SOAP::WSDLDriverFactory.new(url).create_rpc_driver
+	#driver.options['protocol.http.ssl_config.verify_mode'] = OpenSSL::SSL::VERIFY_NONE
+	result= driver.Product_ListRetrieve(:Username=> "rrodriguez@incompanysolutions.com",:Password => "monomono",:SiteId =>100917,:CatalogId =>"-1")
+	#result.inspect
+  object_import(result).inspect
+end
+
+
 
 get '/print' do
   codes = params['codes'];
